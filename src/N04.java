7e6743a7aa64093fd065be5a9e6b269a62028b06
Z109534233 <109534233@stu.ukn.edu.tw>
2023-05-02 14:44:57 +0800

    0502

---
+++ src/N04.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class N04 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int num=sc.nextInt();
+        
+    }
+}
